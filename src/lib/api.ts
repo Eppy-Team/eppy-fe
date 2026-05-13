4df08001f75f50ef0ea826338383bf2daf27a4de
Alif <aldana.goodkids2@gmail.com>
2026-05-13 09:09:11 +0700

    change api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://54.251.135.49:3000/api/v1";
+const BASE_URL = "https://api-eppy.my.id/api/v1";
 
 // ==================== TOKEN ====================
 export const setToken = (token: string) => {
